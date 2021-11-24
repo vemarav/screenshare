cfe0af96999c136f45e2999e264c29021510bb61
vemarav <vemarav@outlook.in>
2021-11-24 22:45:01 +0530

    improved ui

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import {
   View,
@@ -14,10 +14,18 @@ import {
   RTCView, 
   ScreenCapturePickerView,
 } from 'react-native-webrtc';
+import { useIsCaptured } from 'react-native-is-captured';
 
 const App = () => {
   const screenCaptureView = React.useRef(null);
   const [stream, setStream] = useState(null);
+  const isCaptured = useIsCaptured ();
+
+  useEffect(() => {
+    if (!isCaptured) {
+      setStream (null);
+    }
+  }, [isCaptured]);
 
   const startBroadcast = async () => {
     const reactTag = findNodeHandle(screenCaptureView.current);
@@ -39,9 +47,8 @@ const App = () => {
     }
   };
 
-  const stop = () => {
-    stream?.release();
-    setStream(null);
+  const stop = async () => {
+    await startBroadcast();
   };
 
   return (
@@ -61,14 +68,9 @@ const App = () => {
           <ScreenCapturePickerView ref={screenCaptureView}/>
         </View>
         <View style={styles.footer}>
-          <TouchableOpacity onPress={start}>
-            <View style={styles.btn}>
-              <Text style={styles.btnTxt}>Start</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={stop}>
-            <View style={styles.btn}>
-              <Text style={styles.btnTxt}>Stop</Text>
+          <TouchableOpacity onPress={() => isCaptured ? stop() : start() }>
+            <View style={[styles.btn, isCaptured ? styles.active : styles.inactive]}>
+              <Text style={styles.btnTxt}>{isCaptured ? 'Stop' : 'Start'}</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -78,11 +80,13 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  body: {backgroundColor: Colors.dark, ...StyleSheet.absoluteFill},
+  body: {backgroundColor: '#1c1c1c', ...StyleSheet.absoluteFill},
   stream: { flex: 1, padding: 44 },
-  footer: {width: '100%', flexDirection: 'row', justifyContent: 'space-evenly', paddingBottom: 30},
-  btn: {paddingHorizontal: 50, paddingVertical: 10, backgroundColor: '#0275d8', borderRadius: 5},
-  btnTxt: {color: Colors.white, textAlign: 'center', fontSize: 20}
+  footer: {width: '100%', padding: 30},
+  btn: {paddingHorizontal: 50, paddingVertical: 10, borderRadius: 5},
+  btnTxt: {color: Colors.white, textAlign: 'center', fontSize: 20},
+  active: {backgroundColor: '#FC3d39'},
+  inactive: {backgroundColor: '#454545'}
 });
 
 export default App;
